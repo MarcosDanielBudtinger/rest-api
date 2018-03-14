48576670fbd4b2432b9abff0db43a392c64fb91f
Marcos Daniel <marcos.mdbbr@gmail.com>
2018-03-14 10:59:59 -0300

    Created file ValidationMessages.properties for messages of validation for fields;
    Added anotation @NotNull and @Size with min 3 and max 20 for attribute
    nome the entity Categoria;
    
    Returning list of possibles errors in Exception....

--- src/main/java/com/example/rest/api/model/Categoria.java
+++ src/main/java/com/example/rest/api/model/Categoria.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +21,8 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotNull
+	@Size(min = 3, max = 20)
 	private String nome;
 
 	@Override
