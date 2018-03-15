f1abf0275d5b0c610c01aa970d97d364b5a911e3
Marcos Daniel <marcos.mdbbr@gmail.com>
2018-03-15 16:45:00 -0300

    Added depencies for OAuth2 and JWT

--- src/main/java/com/example/rest/api/resource/LancamentoResource.java
+++ src/main/java/com/example/rest/api/resource/LancamentoResource.java
@@ -77,7 +77,7 @@ public class LancamentoResource {
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
-		lancamentoRepository.deleteById(codigo);
+		lancamentoRepository.delete(codigo);
 	}
 
 }
