932d7a44ad67c2312ea0accaea2109e0a3fdc0f0
Marcos Daniel <marcos.mdbbr@gmail.com>
2018-03-19 11:39:17 -0300

    Created filter for entity Pessoa

--- src/main/java/com/example/rest/api/repository/PessoaRepository.java
+++ src/main/java/com/example/rest/api/repository/PessoaRepository.java
@@ -3,8 +3,9 @@ package com.example.rest.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rest.api.model.Pessoa;
+import com.example.rest.api.repository.pessoa.PessoaRepositoryQuery;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>, PessoaRepositoryQuery{
 
 	Pessoa findByCodigo(Long codigo);
 
---
+++ src/main/java/com/example/rest/api/repository/pessoa/PessoaRepositoryQuery.java
@@ -0,0 +1,13 @@
+package com.example.rest.api.repository.pessoa;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.rest.api.model.Pessoa;
+import com.example.rest.api.repository.filter.PessoaFilter;
+
+public interface PessoaRepositoryQuery {
+
+	public Page<Pessoa> filtrar (PessoaFilter pessoaFilter, Pageable pageable);
+	
+}
