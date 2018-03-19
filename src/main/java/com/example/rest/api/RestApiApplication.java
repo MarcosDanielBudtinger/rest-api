8bd4c1dd4c5281d2d78e249999723bee5993a748
Marcos Daniel <marcos.mdbbr@gmail.com>
2018-03-19 09:40:16 -0300

    Added profile for spring

--- src/main/java/com/example/rest/api/RestApiApplication.java
+++ src/main/java/com/example/rest/api/RestApiApplication.java
@@ -2,8 +2,12 @@ package com.example.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.rest.api.config.property.RestApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RestApiProperty.class)
 public class RestApiApplication {
 
 	public static void main(String[] args) {
